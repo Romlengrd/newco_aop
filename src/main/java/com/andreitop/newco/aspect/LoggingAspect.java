9efd2057b7b3b4eb0175dd3f8666ac234e4b70e7
Roman_Samortoshenkov <Roman_Samotoshenkov@epam.com>
2018-09-09 14:22:16 +0300

    AOP

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -1,22 +1,45 @@
 package com.andreitop.newco.aspect;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
 
     @Before("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
     public void beforeRepoFind(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositorySave()")
+    public void afterRepoSave(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @Before("com.andreitop.newco.aspect.PointcutContainer.repositoryUpdate()")
+    public void beforeRepoUpdate(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositoryDelete()")
+    public void afterRepoDelete(JoinPoint joinPoint) {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
---
+++ src/test/java/com/andreitop/newco/repository/RepoSimpleTestLog.java
@@ -0,0 +1,45 @@
+package com.andreitop.newco.repository;
+
+import com.andreitop.newco.dto.TripDto;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RepoSimpleTestLog {
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    private TripDto tripDto = new TripDto();
+
+    @Before
+    public void settrip() {
+        tripDto.setId(1L);
+        tripDto.setOrigin("LLL");
+        tripDto.setDestination("YYY");
+        tripDto.setPrice(3333);
+    }
+
+
+    @Test
+    public void testAdd() {
+        tripRepository.save(tripDto);
+    }
+
+    @Test
+    public void testUpd() {
+        tripDto.setPrice(88888);
+        tripRepository.update(tripDto);
+    }
+
+    @Test
+    public void testDelete() {
+        tripRepository.delete(1L);
+    }
+
+}
